63bc8a2e2904df015948e1e3766d813cbd052b9b
Unubaatar Bodi-Amar <erkaa0337@gmail.com>
2024-12-27 14:53:09 +0800

    add package json

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { Client } = require("pg");
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+const client = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
-const PORT = process.env.PORT || 3000;
+client
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.error("Connection error", err.stack));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+const PORT = process.env.PORT || 4004;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
